933778b368c48232b95ffd780dfa96726a61e3f0
poulhoi <mail@poulhoi.dk>
2021-05-19 10:28:53 +0200

    Adding scripts to auto-increment file name of render

---
+++ Project/phoi_Auto-increment file name of next render and open render settings.lua
@@ -0,0 +1,65 @@
+--[[
+@description phoi_Auto-increment file name of next render and open render settings
+@author Poul Høi
+@links 
+	Repository https://github.com/poulhoi/phoi_ReaScripts
+@version 1.0
+@changelog Initial release
+--]]
+
+-- Finds the number at the end of the render pattern and automatically increments it.
+
+-- NAME
+local scriptName = 'phoi_Auto-increment file name of next render and open render settings'
+
+--FUNCTIONS
+function msg(msg)
+	reaper.ShowConsoleMsg(tostring(msg) .. "\n")
+end
+
+function countDigits(string)
+	local sub = string:match('%d+')
+	return sub:len()
+end
+
+function addLeadingZeros(int, leadingZeros) -- returns as string
+	local int_str = tostring(int)
+	for i = 1, leadingZeros do
+		if math.abs(int) < (10 ^ leadingZeros) then
+			int = int * 10
+			if int >= 0 then
+				int_str = "0" .. int_str
+			else
+				int_str = "-0" .. int_str:gsub('-', '')
+			end
+		end
+	end
+	return int_str
+end
+
+function removeDecimalPoint(str)
+	local decPoint = str:find("%.")
+	if decPoint then str = str:sub(0, decPoint - 1) end
+	return str
+end
+
+function main()
+	local retval, pattern = reaper.GetSetProjectInfo_String(0, 'RENDER_PATTERN', '', false)
+	if not retval then reaper.MB("Render pattern could not be retrieved.", "Error", 0) end
+	local pattern_number = pattern:match('%d*$')
+	if pattern_number == '' then return end
+	local pattern_str = pattern:gsub('%d+$', '')
+	local len = countDigits(pattern_number)
+	local pattern_number_int = tonumber(pattern_number)
+	pattern_number_int = pattern_number_int + 1
+	if len > 0 then
+		pattern_number = addLeadingZeros(pattern_number_int, len-1)
+	else
+		pattern_number = tostring(pattern_number_int)
+	end
+	pattern_number = removeDecimalPoint(pattern_number)
+	reaper.GetSetProjectInfo_String(0, 'RENDER_PATTERN', pattern_str .. pattern_number, true)
+end
+
+main()
+reaper.Main_OnCommand(40015, 0)
